f74669453a9a8520d428ef0dad0a46c9651d6f24
habibshrabon <snhabib777@gmail.com>
2021-11-04 20:27:18 +0600

    server create and database connect

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+//connect
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ewzy7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello doctors portal!");
+});
+
+app.listen(port, () => {
+  console.log(`listening at :${port}`);
+});
